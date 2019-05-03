0f0bad413d4061a62c8ad4b8603dee447a88d32a
cxmgit <458225467@qq.com>
2019-05-03 16:40:31 +0800

    修改本地代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld! 我修改了github的源码哦");
+        System.out.println("HelloWorld! 我修改了本地的源码哦");
     }
 }
