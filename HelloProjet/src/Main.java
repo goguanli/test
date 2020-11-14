c491322aff99b6cea705ae9a535334c8cd7e3c6d
Administrator <458225467@qq.com>
2020-11-14 13:14:42 +0800

    hh

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld! 我修改了本地的源码哦再改一次");
+        System.out.println("HelloWohhhhrllld! 我修改了本地的源码哦再改一次");
     }
 }
