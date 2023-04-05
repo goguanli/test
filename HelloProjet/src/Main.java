cb1f7239e8006f8cda6b10d68b45d06563b0c21a
chenximing <458225467@qq.com>
2023-04-06 00:54:11 +0800

    dev

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -12,12 +12,6 @@ public class Main {
         //我在dev上的修改      ssssssssss
 
 		//555
-        fatal: 'dev' does not appear to be a git repository
-        fatal: Could not read from remote repository.
-
-        Please make sure you have the correct access rights
-                //mmmmmmmmmmmmmmm
-        and the repository exists.
 
         //master1111111
 
