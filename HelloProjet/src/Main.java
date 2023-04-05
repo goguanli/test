ce2d40d744df06f45674090a1d0fe3e9caf69a73
chenximing <458225467@qq.com>
2023-04-06 01:00:08 +0800

    demo修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,16 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWohhhhrllld! 我修改了本地的源码哦再改一次");
         //我在web端的修改
-        //devvvv
-		//我在pc端的修改
-        //main
-        //我在dev上的修改      ssssssssss
-
-		//555
-
-        //master1111111
-
-	   // dasdasd
+    //我是demo
     }
 
 }
